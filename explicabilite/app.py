a23272e24c5cd04e8282732da1a0a4f4a91f2450
sandcha <sandcha@users.noreply.github.com>
2025-11-10 10:20:21 +0100

    Ajoute l'équation logement ordinaire au streamlit

--- explicabilite/app.py
+++ explicabilite/app.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
-from notebooks.situation import situation_18yo_moving_away
-
+from notebooks.situation import situation_18yo_moving_away as situation_to_explain
+# TODO ajouter la période au json aides-simplifiées
 
 st.title("Comprendre le montant d'APL")
 
@@ -13,7 +13,7 @@ st.markdown("Vous avez indiqué demander l'APL pour un **logement en secteur loc
 
 
 # récupération des réponses au questionnaire aides-simplifiées
-answers = situation_18yo_moving_away["answers"]
+answers = situation_to_explain["answers"]
 
 # transcription des réponses d'après le type détecté
 with st.expander("Consulter toutes les réponses fournies au simulateur..."):
@@ -30,3 +30,11 @@ with st.expander("Consulter toutes les réponses fournies au simulateur..."):
 			st.write(f"Champ '{key}': {type(value)} non transcrit.")
 
 
+st.markdown("En secteur locatif ordinaire, l'APL est calculée comme :")
+st.markdown("* un `loyer pris en compte` (`L`) éventuellement plafonné,")
+st.markdown("* additionné d'un `forfait de charges` (`C`),")
+st.markdown("* auquel est retirée une `participation personnelle` (`Pp`) \
+			qui est déterminée en fonction des ressources du ménage et de sa composition familiale.")
+st.markdown("Ainsi le montant d'APL alloué est égal à `L + C - Pp`")
+
+# L est le loyer mensuel réel pris en compte dans la limite d’un plafond variable en fonction de trois zones géographiques et du nombre de personnes à charge.
